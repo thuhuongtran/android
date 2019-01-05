738b6e173af5fa9912ec62ff6a0c4f6dc171ea8e
thuhuongtran <huong.tranthu.hust@gmail.com>
2019-01-05 18:11:21 +0700

    lab6 update

---
+++ lab6/app/src/main/java/com/vtcac/thuhuong/lab6/App.java
@@ -0,0 +1,12 @@
+package com.vtcac.thuhuong.lab6;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
